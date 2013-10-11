0aea9cbcedc39a08722846920e07e9740ef08285
ricksmt <ricksmt@ls.byu.edu>
2013-10-11 12:56:13 -0600

    Initial email setup

---
+++ scripts/email.js
@@ -0,0 +1,52 @@
+(function (global) {
+    var $to,
+        $subject,
+        $body,
+		$anchor,
+       	$button;
+
+    function init() {
+        $to = $("#to");
+        $subject = $("#subject");
+        $body = $("#body");
+    	//$anchor = $("#open-email-client");
+        $button = $("#send-email");
+
+        //$to.on("blur", updateAnchor);
+        //$subject.on("blur", updateAnchor);
+        //$body.on("blur", updateAnchor);
+    	//$anchor.on("click", updateAnchor);
+        $button.on("click", function () {
+        	var to = $to.val().trim(),
+				subject = $subject.val().trim(),
+				body = $body.val().trim();
+
+        	if (to.length === 0 && subject.length === 0 && body.length === 0) return false;
+        	var email = "mailto:" + to;
+        	if (subject.length !== 0 || body.length !== 0) {
+        		$anchor.attr("href", $anchor.attr("href") + '?');
+        		if (subject.length !== 0) $anchor.attr("href", $anchor.attr("href") + "subject=" + subject);
+        		if (subject.length !== 0 && body.length !== 0) $anchor.attr("href", $anchor.attr("href") + '&');
+        		if (body.length !== 0) $anchor.attr("href", $anchor.attr("href") + "body=" + body);
+        	}
+        	window.location.href = email;
+        });
+    }
+
+    $(document).on("deviceready", init);
+
+    function updateAnchor() {
+    	var to = $to.val().trim(),
+    		subject = $subject.val().trim(),
+    		body = $body.val().trim();
+
+    	if (to.length === 0) return false;
+    	$anchor.attr("href", "mailto:" + to);
+    	if (subject.length === 0 && body.length === 0) return true;
+    	$anchor.attr("href", $anchor.attr("href") + '?');
+    	if (subject.length !== 0) $anchor.attr("href", $anchor.attr("href") + "subject=" + subject);
+    	if (subject.length !== 0 && body.length !== 0) $anchor.attr("href", $anchor.attr("href") + '&');
+    	if (body.length !== 0) $anchor.attr("href", $anchor.attr("href") + "body=" + body);
+    }
+
+})(window);
