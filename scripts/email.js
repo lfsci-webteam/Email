3d1bdaaa4328095690b36d3b3d7a27e6cdd6c9a4
ricksmt <ricksmt@ls.byu.edu>
2013-11-15 11:16:30 -0700

    Trying to debug

--- scripts/email.js
+++ scripts/email.js
@@ -13,13 +13,13 @@
         $picture = $("#picture");
         $audio = $("#audio");
         $email = $("#send-email");
+        $image = $audioFile = null;
 
 		// TODO: Picture isn't working either
         $picture.on("click", function () {
         	navigator.camera.getPicture(
 				function onSuccess(imageURI) {
-					getRootDirectory(function (root) { $image = imageURI.replace(root, ''); });
-					getRootDirectory(function (root) { alert(imageURI.replace(root, '')); });
+					getRootDirectory(function (root) { alert($image = imageURI.replace(root, '')); });
 				},
 				function onFail(message) {
 					alert('Failed because: ' + message);
@@ -49,13 +49,15 @@
 				subject = $subject.val().trim(),
 				body = $body.val().trim();
 
-        	if (to.length === 0 && subject.length === 0 && body.length === 0) return false;
         	var attachments = [];
         	if ($image) attachments.push($image);
         	if ($audioFile) attachments.push($audioFile);
+        	alert("Sending email with " + attachments.length + " attachments.");
+
         	window.plugins.emailComposer.showEmailComposerWithCallback(
 				function (arg) { },// <- Note: Function never called. See documentation.
 				subject, body, [to], [], [], false, attachments);
+			alert("Success");
         });
     }
 
